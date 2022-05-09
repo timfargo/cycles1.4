5887c1e0fa1d3215626681ac4c234c0f3ca321fb
timfargo <isalintimur981@gmail.com>
2022-05-09 16:46:40 +0300

    Initial commit

---
+++ src/pro/sky/Main.java
@@ -0,0 +1,74 @@
+package pro.sky;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Задание 1
+//            int j = 1;
+//            while (j <= 10) {
+//                System.out.println(j);
+//                j += 1;
+//                if (j < 10) {
+
+//        for ( int i = 10; i >= 1; i--) {
+//            System.out.println(i);
+
+        // Задание 2
+//            int i = 7;
+//            while (i <= 31) {
+//                System.out.println("Сегодня пятница 7 число. Необходимо подготовить отчет");
+//                i += 7;
+//                if (i < 7);
+
+        // Задание 3
+           int i = 79;
+           while (i<=2054){
+               System.out.println(i+"");
+               i+=79;
+
+               }
+           }
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
